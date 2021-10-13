15e391c7f9279343bb3fb4de319ddfe11f49a527
Rajendra <rajendradanve@gmail.com>
2021-10-13 11:57:48 +0000

    sorting updated

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.db.models.functions import Lower
 
 
 # Create your views here.
@@ -22,7 +23,7 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey == 'category__name'
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
