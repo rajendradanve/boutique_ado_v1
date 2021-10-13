73eb012b1a541761c1ee6d3b931c3f125aba718c
Rajendra <rajendradanve@gmail.com>
2021-10-13 11:30:46 +0000

    sorting updated

--- products/views.py
+++ products/views.py
@@ -11,7 +11,7 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
-    sort= None
+    sort = None
     direction = None
 
     if request.GET:
@@ -21,6 +21,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey == 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
@@ -28,8 +30,6 @@ def all_products(request):
                     sortkey = f'-{sortkey}'
 
             products = products.order_by(sortkey)
-
-
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
