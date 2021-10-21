3fd36c9787bc8d3691890dcfdddf756205d1b631
Rajendra <rajendradanve@gmail.com>
2021-10-21 10:42:37 +0000

    Added checkout success logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from .forms import OrderForm
-from bag.contexts import bag_contents
 from django.conf import settings
-import stripe
+
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+from products.models import Product
+from bag.contexts import bag_contents
 
-# Create your views here.
+import stripe
 
 
 def checkout(request):
-
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -30,7 +29,6 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-
         if order_form.is_valid():
             order = order_form.save()
             for item_id, item_data in bag.items():
@@ -70,7 +68,7 @@ def checkout(request):
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
-        
+
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
@@ -82,37 +80,34 @@ def checkout(request):
 
         order_form = OrderForm()
 
-       
-
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
-
     }
 
     return render(request, template, context)
 
 
 def checkout_success(request, order_number):
-    """ 
+    """
     Handle successful checkouts
     """
-
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
-            will be sent to {order.email}.')
-    
-    if 'bag' in request.session: 
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
         del request.session['bag']
 
-    template = 'checkout/checkout_sucess.html'
+    template = 'checkout/checkout_success.html'
     context = {
         'order': order,
     }
