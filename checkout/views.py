a6c66f9ddb38713036ff51c6099419655c619353
Rajendra <rajendradanve@gmail.com>
2021-10-23 21:29:52 +0000

    updated webhook handler to handle profile

--- checkout/views.py
+++ checkout/views.py
@@ -147,7 +147,6 @@ def checkout_success(request, order_number):
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
-
         order.user_profile = profile
         order.save()
 
@@ -161,13 +160,10 @@ def checkout_success(request, order_number):
                 'default_street_address1': order.street_address1,
                 'default_street_address2': order.street_address2,
                 'default_county': order.county,
-           
             }
-
             user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid:
+            if user_profile_form.is_valid():
                 user_profile_form.save()
-
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
